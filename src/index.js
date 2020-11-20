11963dfed04bb3bad6b08249b414b8195d34ad20
DTHutton <48737212+DTHutton@users.noreply.github.com>
2020-11-20 14:47:15 -0500

    added css sheets, header, and footer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
+/**
+ * selectors for two main sections
+ * primarily used for adding/removing hide class
+ */
 const form = document.querySelector('.form-container');
-const burrito = document.querySelector('.burrito');
+const results = document.querySelector('.results');
+
+/**
+ * selectors for buttons
+ */
 const calcBtn = document.querySelector(`input[type='submit']`);
-const formInputs = document.querySelectorAll(`input[type='text']`);
+const recalculate = document.querySelector('#recalculate');
+
+/**
+ * selectors for results elements
+ */
 const paycheckResults = document.querySelector('#paycheckResults');
 const billsResults = document.querySelector('#billsResults');
 const jointResults = document.querySelector('#jointResults');
 const savingsResults = document.querySelector('#savingsResults');
 const spendingResults = document.querySelector('#spendingResults');
-const recalculate = document.querySelector('#recalculate');
+
+/**
+ * selector for grabbing form data
+ * initialized object that form data is stored in
+ */
+const formInputs = document.querySelectorAll(`input[type='text']`);
 const formValues = {};
 
+/**
+ * footer selector
+ * copyright code
+ */
+const footer = document.querySelector('footer');
+const copyrightDate = new Date().getFullYear();
+footer.textContent = `Copyright © ${copyrightDate} DTHutton`;
+
+/**
+ * grabs form info
+ * calls the function to calculate results
+ * resets and hides form
+ * shows results elements
+ * @param {*} event 
+ */
 function runApp(event) {
     event.preventDefault();
     formInputs.forEach(({ id, value }) => formValues[id] = Number(value));
     form.reset();
     form.classList.add('hide');
-    burrito.classList.remove('hide');
+    results.classList.remove('hide');
     calculate(formValues);
 }
 
+/**
+ * takes form data as an argument an sets the values in the results fields
+ * @param {object} formValues 
+ */
 function calculate({ paycheck, bills, joint, savings }) {
     let total;
     let amountToJoint;
@@ -38,18 +74,30 @@ function calculate({ paycheck, bills, joint, savings }) {
     spendingResults.textContent = `$${adjust(total)}`;
 }
 
+/**
+ * returns arg as a decimal representing a percentage
+ * @param {number} num 
+ */
 function convertToPercentage(num) {
     return Math.floor(num) / 100;
 }
 
+/**
+ * returns arg rounded down to two decimals places
+ * @param {number} num 
+ */
 function adjust(num) {
     return Math.floor(num * 100) / 100;
 }
 
+/**
+ * resets app
+ * @param {*} event 
+ */
 function mulligan(event) {
     event.preventDefault();
     form.classList.remove('hide');
-    burrito.classList.add('hide');
+    results.classList.add('hide');
     paycheckResults.textContent = `$0.00`;
     billsResults.textContent = `$0.00`;
     jointResults.textContent = `$0.00`;
@@ -57,5 +105,8 @@ function mulligan(event) {
     spendingResults.textContent = `$0.00`;
 }
 
+/**
+ * event listeners for buttons
+ */
 calcBtn.addEventListener('click', runApp);
 recalculate.addEventListener('click', mulligan);
